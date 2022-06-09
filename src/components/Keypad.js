1144071abb2b42164f23784b8be5551ec0d27e1b
Qibin <qbye0405@gmail.com>
2022-06-09 11:44:09 -0400

    commit tree f054a576d9b66e955732cca951618ea8245fa928

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+
+function handleChange(event){
+    return event.target.value
+}
+
+function keypad(){
+
+    return (
+    <div>
+        <input type='text' onChange={handleChange} value="Entering password">Password</input>
+    </div>
+        
+    )
+}
+
+
+export default keypad
